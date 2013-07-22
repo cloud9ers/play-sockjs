e0574780fa3a0b0e19eed7c4d1dbeafc95368386
Mostafa Sameh <msameh@cloud9ers.com>
2013-07-22 16:51:57 +0200

    import the plugin in the samples project and sockjs config

--- samples/play-sockjs-sample/project/Build.scala
+++ samples/play-sockjs-sample/project/Build.scala
@@ -7,8 +7,10 @@ object ApplicationBuild extends Build {
   val appName         = "play-sockjs-sample"
   val appVersion      = "1.0-SNAPSHOT"
 
+  val sockjs = "play-sockjs" % "play-sockjs_2.10" % "1.0-SNAPSHOT"
+
   val appDependencies = Seq(
-    // Add your project dependencies here,
+    sockjs,
     jdbc,
     anorm
   )
