685b38b3fe45400bcfa474dbe39880d8eb983391
Mostafa Sameh <msameh@cloud9ers.com>
2013-07-23 14:08:44 +0200

    route maching trial

---
+++ samples/play-sockjs-sample/app/controllers/SockJsService.scala
@@ -0,0 +1,10 @@
+package controllers
+
+import play.api.mvc.Controller
+import com.cloud9ers.play2.sockjs.SockJs
+
+object SockJsService extends Controller with SockJs {
+
+  def sockJs(route: String) = sockJsHandler(route)
+
+}
