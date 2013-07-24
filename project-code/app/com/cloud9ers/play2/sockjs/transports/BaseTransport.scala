8104cf97955144c3816eb76bd06c3d1d940dfcc8
Amal Elshihaby <ashihaby@cloud9ers.com>
2013-07-24 10:22:38 +0200

    Add transports package

---
+++ project-code/app/com/cloud9ers/play2/sockjs/transports/BaseTransport.scala
@@ -0,0 +1,11 @@
+package com.cloud9ers.play2.sockjs.transports
+
+object BaseTransport {
+  
+}
+class BaseTransport {
+  /*
+   * Implements few methods that session expects to see in each transport
+   */
+
+}
---
+++ project-code/app/com/cloud9ers/play2/sockjs/transports/RawWebSocketTransport.scala
@@ -0,0 +1,8 @@
+package com.cloud9ers.play2.sockjs.transports
+
+class RawWebSocketTransport extends BaseTransport {
+  /*
+   *  Raw websocket transport implementation
+   */
+
+}
