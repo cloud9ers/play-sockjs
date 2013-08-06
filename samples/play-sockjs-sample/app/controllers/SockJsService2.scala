951c44d16bf7cab75cee1a6ee6fdba59af075ad3
Mostafa Sameh <msameh@cloud9ers.com>
2013-08-06 16:26:38 +0200

    xhr_streaming

--- samples/play-sockjs-sample/app/controllers/SockJsService2.scala
+++ samples/play-sockjs-sample/app/controllers/SockJsService2.scala
@@ -88,8 +88,9 @@ object SockJsService2 extends Controller {
    * According to the transport, it creates the sockjs Enumerator/Iteratee and return Handler in each path
    * calls enqueue/dequeue of the session to handle msg queue between send and receive
    */
+  val H_BLOCK = ((for (i <- 0 to 2047) yield "h").toArray :+ "\n").reduceLeft(_ + _).toArray.map(_.toByte)
   def handler[A](f: RequestHeader => (Enumerator[A], Iteratee[A, Unit]) => Unit) = {
-    if (true) Action { implicit request => // Should match handler type (Actoin, Websocket .. etc)
+    if (true) Action { implicit request => // Should match handler type (Action, Websocket .. etc)
       println(request.path)
       val Array(_, _, serverId, sessionId, transport) = request.path.split("/")
       transport match {
@@ -100,6 +101,28 @@ object SockJsService2 extends Controller {
                 CONTENT_TYPE -> "application/javascript;charset=UTF-8",
                 CACHE_CONTROL -> "no-store, no-cache, must-revalidate, max-age=0")
               .withHeaders(cors: _*)))
+        case "xhr_streaming" =>
+          val (enum, channel) = Concurrent.broadcast[Array[Byte]]
+          val enum1 = Enumerator(H_BLOCK.toArray.map(_.toByte), "o\n".toArray.map(_.toByte), "a[\"x\"]\n".toArray.map(_.toByte))
+          import java.io.InputStream
+          val enum2 = Enumerator.fromStream(new InputStream() { def read = ??? }, 100)
+          val result = Ok.stream(enum)
+            .withHeaders(
+              CONTENT_TYPE -> "application/javascript;charset=UTF-8",
+              CACHE_CONTROL -> "no-store, no-cache, must-revalidate, max-age=0")
+            .withHeaders(cors: _*)
+          Future {
+            Thread.sleep(1000)
+            channel push H_BLOCK
+//            channel push "o\n".toArray.map(_.toByte)
+//            channel push "a[\"x\"]\n".toArray.map(_.toByte)
+            getOrCreateSession(sessionId).send { ms =>
+              println(ms)
+              channel push ms.toArray.map(_.toByte)
+            }
+          }
+          //          channel push H_BLOCK.toArray.map(_.toByte)
+          result
         case "xhr_send" =>
           val (upEnumerator, upChannel) = Concurrent.broadcast[A]
           val downIteratee = Iteratee.foreach[A] { userMsg =>
@@ -109,7 +132,7 @@ object SockJsService2 extends Controller {
           f(request)(upEnumerator, downIteratee)
           //request.body.asText.map { m => println(m); (upChannel push m.asInstanceOf[A]) }
           //FIXME map doesn't happen, the body is not text/plain, 7asbia Allah w ne3ma el wakel :@
-          upChannel push "x".asInstanceOf[A]
+          upChannel push "a[\"x\"]\n".asInstanceOf[A]
           NoContent
             .withHeaders(
               CONTENT_TYPE -> "text/plain;charset=UTF-8",
@@ -132,13 +155,14 @@ object SockJsService2 extends Controller {
   class Session(sessionId: String) {
     trait Event
     case class Msg(msg: String) extends Event
-    case class ReadyToWrite(p: scala.concurrent.Promise[String]) extends Event
+    type Writer = String => Unit
+    case class ReadyToWrite(writer: Writer) extends Event
     /**
      * Structure to keep track of iteration state
      * @param ms - list of queued messages waiting to be sent to the sockjs client
      * @param writablePromise - promise will be used to send data on it's onsuccess callback
      */
-    case class Accumulator(ms: List[String], writablePromise: Option[Promise[String]])
+    case class Accumulator(ms: List[String], writer: Option[Writer])
     // for queuing the messages to be flushed when the downstream connection is ready 
     private[this] val (msgEnumerator, msgChannel) = Concurrent.broadcast[Event]
 
@@ -151,21 +175,21 @@ object SockJsService2 extends Controller {
           // send the msg to the next step or flush if the downstream connection is ready
           case Msg(msg) =>
             println(msg)
-            acc.writablePromise match {
-              case Some(p) => sendFrame(acc.ms :+ msg, p) //TODO test for: check if the down channel is ready and a new msg received
+            acc.writer match {
+              case Some(writer) => sendFrame(acc.ms :+ msg, writer) //TODO test for: check if the down channel is ready and a new msg received
               case None => Cont(i => step(Msg(msg), Accumulator(acc.ms :+ msg, None))(i)) //TODO optimize append to the right
             }
           // takes a promise and fulfill that promise if the msg queue has items or send ready state to the next step otherwise
-          case ReadyToWrite(p) => //TODO test for: check if the ms is not empty and send the p to next step
+          case ReadyToWrite(writer) => //TODO test for: check if the ms is not empty and send the p to next step
             acc.ms match {
               case Nil => Cont(i => step(Msg(""), acc)(i))
-              case ms => sendFrame(ms, p)
+              case ms => sendFrame(ms, writer)
             }
         }
       }
-      def sendFrame(ms: List[String], p: Promise[String])(implicit i: Input[Event]): Iteratee[Event, Accumulator] = {
-        p success encodeMsgs(ms.filterNot(_ == ""))
-        Cont(i => step(Msg(""), Accumulator(Nil, None))(i))
+      def sendFrame(ms: List[String], writer: String => Unit)(implicit i: Input[Event]): Iteratee[Event, Accumulator] = {
+        writer(encodeMsgs(ms.filterNot(_ == "")))
+        Cont(i => step(Msg(""), Accumulator(Nil, None))(i)) //FIXME: None resets the writer each time which is not suitable for streaming
       }
       Cont(i => step(Msg(""), Accumulator(Nil, None))(i))
     }
@@ -178,13 +202,25 @@ object SockJsService2 extends Controller {
      */
     def dequeue(): Future[String] = {
       val p = Promise[String]()
-      msgChannel push ReadyToWrite(p) // send ReadyToWrite to the msgIteratee with the promise that will be used to return the msgs
+      msgChannel push ReadyToWrite(ms => p success ms) // send ReadyToWrite to the msgIteratee with the promise that will be used to return the msgs
       p.future
     }
     /**
      * adds a message to message queue
      */
-    def enqueue(msg: String) = msgChannel push Msg(msg) // pushes a message to the message queue
+    def enqueue(msg: String) = {
+      msgChannel push Msg(msg) // pushes a message to the message queue
+      this
+    }
+    /**
+     * send the queued messages using the writer function when the downstream channel is ready
+     * @param writer - function takes the msg and write it according to the transport
+     */
+    def send(writer: String => Unit) = {
+      msgChannel push ReadyToWrite(writer)
+      this
+    }
+
   }
 
   //TODO find a more decent way to store sessions
@@ -195,7 +231,9 @@ object SockJsService2 extends Controller {
   def getOrCreateSession(sessionId: String): Session = sessions.get(sessionId).getOrElse {
     val session = new Session(sessionId)
     sessions put (sessionId, session)
-    session.enqueue("o\n")
+    session
+//    	.enqueue(H_BLOCK)
+    	.enqueue("o\n")
     session
   }
 }
